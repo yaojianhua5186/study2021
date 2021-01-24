741283636a304007cec325615ee499ebf1ede38f
yaojianhua5186 <358036541@qq.com>
2021-01-24 12:22:09 +0800

    修改内容

--- redis-client/src/main/java/com/yaojianhua/redis/RedisClientMain.java
+++ redis-client/src/main/java/com/yaojianhua/redis/RedisClientMain.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class RedisClientMain {
     public static void main(String[] args) {
